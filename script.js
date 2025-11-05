30a0ecb8a292b97410fe1cc6867c529329f7a958
radodoo-cloud <radodoo@gmail.com>
2025-11-05 11:42:21 -0500

    Modify card image paths and fix typos
    Updated image paths for card assets and fixed a typo in card4's image path.

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
+        { name: 'card1', img: 'images/distracted1.png' },
+        { name: 'card1', img: 'images/distracted1.png' },
+        { name: 'card2', img: 'images/drake1.png' },
+        { name: 'card2', img: 'images/drake1.png' },
+        { name: 'card3', img: 'images/fine1.png' },
+        { name: 'card3', img: 'images/fine1.png' },
+        { name: 'card4', img: 'images/rollsafe1.png' },
+        { name: 'card4', img: 'images/rollsaf1e.png' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
         // ...add more pairs as needed
@@ -75,3 +75,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     startButton.addEventListener('click', createBoard);
 });
+
